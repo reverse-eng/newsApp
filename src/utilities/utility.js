1e9976e46269bf2f5e41456bbda53128884cf834
reverse-eng <anton.davis302@gmail.com>
2021-03-19 10:37:41 -0400

    Added access-control-allow-origin to header of git request

--- src/utilities/utility.js
+++ src/utilities/utility.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://newsapi.org/v2/top-headlines/",
     responseType: "json",
-    headers: {'X-Api-Key' : 'a9cd0a05f40f476d8bca878cdbac8330'}
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'X-Api-Key' : 'a9cd0a05f40f476d8bca878cdbac8330'
+    }
   });
