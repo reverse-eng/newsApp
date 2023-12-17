9f170297b73f18c92a3cb788d642738d64f6325e
reverse-eng <58488617+reverse-eng@users.noreply.github.com>
2023-12-17 13:42:16 -0500

    Update utility.js

--- src/utilities/utility.js
+++ src/utilities/utility.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://newsapi.org/v2/",
+    baseURL: "https://northwind-demo.tenantreview.workers.dev/api/articles",
     responseType: "json"
   });
