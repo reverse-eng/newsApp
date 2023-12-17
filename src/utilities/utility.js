04cea0be764b12f07fd8b2d3cb83f2c982648ae7
reverse-eng <58488617+reverse-eng@users.noreply.github.com>
2023-12-17 12:41:21 -0500

    Update utility.js

--- src/utilities/utility.js
+++ src/utilities/utility.js
@@ -2,11 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://newsapi.org/v2/",
-    responseType: "json",
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-      "Access-Control-Max-Age": "86400",
-      "X-Api-Key": "a9cd0a05f40f476d8bca878cdbac8330"
-    }
+    responseType: "json"
   });
