ca636901d3c8209d55fa78fc04309ec0accb4ded
calithemandc <khalil.boyd@gmail.com>
2023-12-17 14:22:00 -0500

    add google site verification meta tag

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -4,9 +4,6 @@ import Navigation from './Navigation'
 const Layout = (props) => {
     return (
         <>
-        <head>
-            <meta name="google-site-verification" content="1xzEhvkWyJpTbkKeasuxKb53beF10mCVZjzsBIzpee0" />
-        </head>
         <div>
             <Navigation />
             {props.children}
