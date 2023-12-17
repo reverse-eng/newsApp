05aacfdce647b04207c492031fbfdc4aa1df09ac
reverse-eng <58488617+reverse-eng@users.noreply.github.com>
2023-12-17 12:05:02 -0500

    Update utility.js

--- src/utilities/utility.js
+++ src/utilities/utility.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://newsapi.org/v2/top-headlines/",
+    baseURL: "https://newsapi.org/v2/top-headlines?country=us&apiKey=a9cd0a05f40f476d8bca878cdbac8330",
     responseType: "json",
     headers: {
       'Access-Control-Allow-Origin': '*',
