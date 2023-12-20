8473c8447a83e604ed60c63947f1c6c59274190c
calithemandc <khalil.boyd@gmail.com>
2023-12-20 14:17:00 -0500

    update view for original articles

--- src/components/viewNewsArticle/ViewNewsArticle.js
+++ src/components/viewNewsArticle/ViewNewsArticle.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import { Container, Row, Col, Button } from "shards-react";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import './viewnewsarticle.css'
 
-const ViewNewsArticle = (props) => {
+const ViewNewsArticle = ({ article }) => {
     console.log('this is loaded')
-    const {Article} = props;
+    // const {Article} = props;
     return(
         <Container fluid className="news-article-container">
             
             <Row>
             <Col sm="12" lg="12">
-                <h1 className="article-title">{Article.article.title}</h1>
+                <h1 className="article-title">{article.title}</h1>
                 
             </Col>
             </Row>
             <Row>
                 <Col lg="2" ></Col>
                 <Col className="img-col" sm="12" lg="8">
-                    <img className="news-art-img" alt=" " src={Article.article.urlToImage} />
+                    <img className="news-art-img" alt=" " src={article.urlToImage} />
                 </Col>
                 <Col lg="2"></Col>
             </Row>
             <Row>
                 <Col sm="12" lg="6">
-                    {Article.article.source.name} 
+                    {article.source.name} 
                 </Col>
                 <Col sm="12" lg="6">
-                    {Article.article.author}
+                    {article.author}
                 </Col>
             </Row>
             <Row>
-                {Article.article.content}
-                
+                {ReactHtmlParser(article.content)}
             </Row>
-            <Row>
-                <a href={Article.article.url}>Read full story at {Article.article?.source?.source?.name}</a>
-            </Row>
+            {/* <Row>
+                <a href={article.url}>Read full story at {article?.source?.source?.name}</a>
+            </Row> */}
 
-            <Button onClick={props.BackButton} theme="dark"> <span className="back-arrow"> &larr;</span></Button>
+            {/* <Button onClick={props.BackButton} theme="dark"> <span className="back-arrow"> &larr;</span></Button> */}
         </Container>
     )
 }
--- src/containers/NewsDetails.js
+++ src/containers/NewsDetails.js
@@ -1,10 +1,19 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import ViewNewsArticle from '../components/viewNewsArticle/ViewNewsArticle';
+import utility from '../utilities/utility';
+import { useParams } from "react-router-dom";
 
 const NewsDetails = () => {
+    const [article, setArticle] = useState(null)
+    const { slug } = useParams();
+    useEffect(async () =>{
+        const articleSlug = await utility.get(`/article/${slug}`)
+        setArticle(articleSlug.data[0])
+    }, []);
+
     return <div>
 
-        This is it
+       {!article ? '...Loading' :  <ViewNewsArticle article={article} />}
     </div>
 }
 
